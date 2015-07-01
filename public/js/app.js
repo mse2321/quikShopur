e798c36bcd20bfa55fbb052fa447bee3461d7e33
mse2321 <mse233@gmail.com>
2015-07-01 18:19:05 -0400

    making updates to js code to tailor to app

--- public/js/app.js
+++ public/js/app.js
@@ -7,33 +7,23 @@ $(document).ready( function() {
 	});*/
 		
 	//Add Info Handler
-	$('#submit').on('tap', charInfoStorage);
+	$('#addButton').on('tap', charInfoStorage);
 			
-	function charInfoStorage(totalXp){		
+	function charInfoStorage(){		
 		var charName = $('#charName').val();
-		var charLevel = parseInt($('#charLevel').val());
-		var totalCharXp = totalXp;
 		$("#newTotalXp").html(totalCharXp);
 		
 		var charInfo = charName;
-		var charInfo2 = totalCharXp;
-		var charInfo3 = charLevel;
 			
 		var name = new Array();
-		var xp = new Array();
-		var level = new Array();
 			
 		name.push(charInfo);
-		xp.push(charInfo2);
-		level.push(charInfo3);
 		
 		// Set stringified object to localStorage
 		localStorage.setItem('name',name);
-		localStorage.setItem('xp',xp);
-		localStorage.setItem('level',level);
 		
 		// Redirect
-		window.location.href="xpCalculator.html";		
+		window.location.href="index.html";		
 		return false;
 	}
 	
