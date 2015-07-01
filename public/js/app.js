bd5d3945e1d3673cf6b84f34848da35ff87b2542
mse2321 <mse233@gmail.com>
2015-07-01 17:15:29 -0400

    added xp calculator code that fits this project

--- public/js/app.js
+++ public/js/app.js
@@ -5,10 +5,43 @@ $(document).ready( function() {
 	/*$("#list-items").click(function(this){
 		$("span[class='removeItem']").show(this);
 	});*/
-
-
-
-
+		
+	//Add Info Handler
+	$('#submit').on('tap', charInfoStorage);
+			
+	function charInfoStorage(totalXp){		
+		var charName = $('#charName').val();
+		var charLevel = parseInt($('#charLevel').val());
+		var totalCharXp = totalXp;
+		$("#newTotalXp").html(totalCharXp);
+		
+		var charInfo = charName;
+		var charInfo2 = totalCharXp;
+		var charInfo3 = charLevel;
+			
+		var name = new Array();
+		var xp = new Array();
+		var level = new Array();
+			
+		name.push(charInfo);
+		xp.push(charInfo2);
+		level.push(charInfo3);
+		
+		// Set stringified object to localStorage
+		localStorage.setItem('name',name);
+		localStorage.setItem('xp',xp);
+		localStorage.setItem('level',level);
+		
+		// Redirect
+		window.location.href="xpCalculator.html";		
+		return false;
+	}
+	
+	function clearInfo(){
+		localStorage.clear();
+		alert('Character Information Cleared');
+		window.location.href="xpCalculator.html";
+	}
 
 
 
