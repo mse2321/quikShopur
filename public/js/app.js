1fb94532110659149aa903b81f815b6b36e0983d
mse2321 <mse233@gmail.com>
2015-07-03 13:52:41 -0400

    adjusted js script almost works correctly

--- public/js/app.js
+++ public/js/app.js
@@ -1,38 +1,28 @@
 $(document).ready( function() {
 
-	$("span[class='removeItem']").hide();
+	/*$("span[class='removeItem']").hide(); */
 
 	/*$("#list-items").click(function(this){
 		$("span[class='removeItem']").show(this);
 	});*/
-		
-	//Add Info Handler
-	$('#addButton').on('tap', charInfoStorage);
-			
-	function charInfoStorage(){		
-		var charName = $('#charName').val();
-		$("#newTotalXp").html(totalCharXp);
-		
-		var charInfo = charName;
-			
-		var name = new Array();
-			
-		name.push(charInfo);
-		
-		// Set stringified object to localStorage
-		localStorage.setItem('name',name);
-		
-		// Redirect
-		window.location.href="index.html";		
-		return false;
-	}
 	
-	function clearInfo(){
-		localStorage.clear();
-		alert('Character Information Cleared');
-		window.location.href="xpCalculator.html";
-	}
+	$("#item").focusout(function(itemVal)	{
+		itemVal = $('#item').val();
+	$('#item').data('itemVal', itemVal);
+		console.log(itemVal);
+	});
 
+	//Add Info Handler
+	$('button[id="addButton"]').on('click', itemStorage);
 
+	function itemStorage(itemVal){		
+		//itemList = []
+		itemName = $("#item").data('itemVal');
+
+		$("#list-items").each(function() {
+			$("#list-items").html("<li><span class='removeItem'>X</span>" + itemName + "</li>");
+		});
+			
+	};
 
 });
